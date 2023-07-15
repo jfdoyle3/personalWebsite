8674e1eb07f6f2e395945dbbbf364717e3f39f8b
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-07-14 22:24:38 -0400

    switched from a Gradle project to a Maven project

---
+++ personal_website/src/test/java/org/jfdeveloper/personal_website/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jfdeveloper.personal_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
